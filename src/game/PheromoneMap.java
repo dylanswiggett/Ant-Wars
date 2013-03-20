c2caaa3822c10e59f8117b38dcf2cf100e31226e
Ryan Russell <russellsprouts2@gmail.com>
2013-03-20 01:29:19 -0700

    Initial Pheromone Map

---
+++ src/game/PheromoneMap.java
@@ -0,0 +1,15 @@
+package game;
+
+class PheromoneMap {
+  enum Pheromones {TRAIL, FOOD, ALARM};
+	
+	
+	public PheromoneMap(){
+	}
+	
+	class PheromoneNode{
+	  Pheromones type;
+		PheromoneNode parent;
+		
+	}
+}
